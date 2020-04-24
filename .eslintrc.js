97467b4d28b3351a59644220022acd63eeb172ab
manaki <mana23happy@hotmail.co.jp>
2020-04-25 01:39:52 +0900

    :+1:eslint導入

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,16 +8,21 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended'
   ],
+  // *.vue ファイルを lint にかけるために必要
   plugins: [
-    'prettier'
+    'vue'
   ],
-  // add your custom rules here
+  // ここにカスタムルールを追加します。
   rules: {
+    "vue/html-self-closing": 'off',
+    'semi': [2, 'never'],
+    'vue/no-v-html': 'off',
+    'no-console': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'prettier/prettier': ['error', { 'semi': false }]
   }
 }
