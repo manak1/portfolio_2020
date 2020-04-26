c400d9139d0218fc065e7c019e3d0f7ecc5ffd15
manaki <mana23happy@hotmail.co.jp>
2020-04-27 01:31:47 +0900

    :+1:fullpage.js導入

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,28 @@
 <template>
-  <div></div>
+  <client-only>
+    <full-page :options="options">
+      <div></div>
+    </full-page>
+  </client-only>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      options: {
+        licenseKey: 'YOUR_KEY_HERE',
+        menu: '#menu',
+        anchors: ['page1', 'page2', 'page3'],
+        scrollingSpeed: 800,
+        afterLoad: this.afterLoad,
+        animateAnchor: true,
+        navigation: true,
+        navigationPosition: 'left',
+        onLeave: this.onMove,
+        easingcss3: 'cubic-bezier(0.77, 0.2, 0.05, 1)'
+      }
+    }
+  }
+}
 </script>
